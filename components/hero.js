b0db6c6bd6a4bfdfb412cbafe7774d0724e7a277
itc-n22006 <n22006@std.it-college.ac.jp>
2023-01-23 15:33:44 +0000

    CodePrep

---
+++ components/hero.js
@@ -0,0 +1,34 @@
+import Link from 'next/link'
+
+export default function Hero () {
+  return (
+    <>
+      <h1>CodePrep</h1>
+      <Link href='/page1' legacyBehavior>
+        <a>Sample Website1</a>
+      </Link>
+
+      <p><Link href='/page2' legacyBehavior>
+        <a>Sample Website2</a>
+         </Link>
+      </p>
+
+      <p><Link href='/page3' legacyBehavior>
+        <a>Sample Website3</a>
+         </Link>
+      </p>
+
+      <p><Link href='/page4' legacyBehavior>
+        <a>Sample Website4</a>
+         </Link>
+      </p>
+
+      <p><Link href='/page5' legacyBehavior>
+        <a>Sample Website5</a>
+         </Link>
+      </p>
+
+    </>
+
+  )
+}
---
+++ pages/page4.js
@@ -0,0 +1,16 @@
+import styles from 'styles/page4.module.css'
+
+export default function Hero() {
+  return (
+    <>
+
+		<div className={styles.body}>
+		<div className={styles.paper}>
+		<p>PAPER</p>
+		</div>
+		</div>
+
+    </>
+  )
+}
+
